0906ed08358959312e988a223e33b45c9d63f503
Kresh195 <KreshF5@mail.ru>
2025-06-25 04:37:55 +0700

    Add dummy classes

---
+++ Lab3/DummyDatabaseController_LoginExists.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class DummyDatabaseController_LoginExists : IDatabaseController
+    {
+        public IDoctorEntry getNewDoctorEntry() { throw new NotImplementedException(); }
+        public bool login(string login, string password) { return true; }
+        public bool tryConnectDB() { return true; }
+        public bool tryCreateAccount(string login, string password) { return false; }
+    }
+
+}
---
+++ Lab3/DummyDatabaseController_OK.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class DummyDatabaseController_OK : IDatabaseController
+    {
+        public IDoctorEntry getNewDoctorEntry()
+        {
+            return new DummyDoctorEntry()
+            {
+                ID = "1",
+                Login = "myname_doctor",
+                Password = "DoctorSuperBest123!"
+            };
+        }
+        public bool login(string login, string password) { return true; }
+        public bool tryConnectDB() { return true; }
+        public bool tryCreateAccount(string login, string password) { return true; }
+    }
+
+}
